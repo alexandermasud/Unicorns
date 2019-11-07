ea2b2fc407e2c22c070e01f37ffa5235a5a1143f
Alexander Masud <alexandermasud@gmail.com>
2019-11-07 19:47:02 +0100

    Fetches and displays a single unicorn

--- index.php
+++ index.php
@@ -51,6 +51,25 @@ $data = json_decode($res->getBody(), true);
                 </div>
             </div>
         </form>
+
+        <?php
+        // Checks if id is set in url
+        // If it's set, a specific unicorn is fetched
+        if (isset($_GET['id'])) {
+            echo   "<div class='row'>
+                        <div class='col-6'>
+                            <h1>" . $data['name'] . "</h1>
+                            <p>" . $data['spottedWhen'] . "</p>
+                            <p>" . $data['description'] . "</p>
+                            <p> <b>Rapporterad av: </b> " . $data['reportedBy'] . "</p>
+                        </div>
+                        <div class='col-6'>
+                            <img src= " . $data['image'] . "  alt='Bild på " . $data['name'] . "' class='img-fluid'>
+                        </div>
+                    </div>";
+        } else {};
+        echo "</table>";
+        ?>
     </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
